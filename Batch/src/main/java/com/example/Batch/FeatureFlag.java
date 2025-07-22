3535a67a352a400592343a079845dcf8a14af4e3
Arunkumar2205 <147905123+Arunkumar387@users.noreply.github.com>
2025-07-22 12:00:02 +0530

    Add files via upload

---
+++ Batch/src/main/java/com/example/Batch/FeatureFlag.java
@@ -0,0 +1,57 @@
+package com.example.Batch;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class FeatureFlag {
+
+
+    @Value("${flag.key.value}")
+    private static String FLAG;
+
+
+    public static void main(String[] args) {
+        boolean toggled = true;
+        boolean featureEnabled = FLAG.equals("DEPRECATED_FLAG"); // DEPRECATED_FLAG
+        if (featureEnabled) {
+            System.out.println("This feature is enabled.");
+        }
+        else{
+            System.out.println("this feature is disabled");
+        }
+
+        if(featureEnabled && toggled){
+            System.out.println("toggled");
+        }
+        else {
+            System.out.println("not toggled");
+        }
+
+        boolean result  = isFlag(featureEnabled);
+        System.out.println(result);
+
+        if (featureEnabled) {
+            try {
+                String realmId = null;
+                if (toggled) {
+                    System.out.println(realmId);
+                }
+                if (toggled) {
+                    System.out.println("topicName");
+                } else {
+                    System.out.println("message");
+                }
+            } catch (Exception e) {
+                throw e;
+            }
+        } else {
+            System.out.println("Skipping publishing BRM Event to KAFKA Topic as toggle AMO_KAFKA_ENABLED is disabled");
+        }
+        System.out.println(
+                "Skipping publishing BRM Event to KAFKA Topic as toggle AMO_KAFKA_ENABLED is disabled");
+    }
+
+    public static boolean isFlag(boolean feature){
+        return feature;
+    }
+}
+
